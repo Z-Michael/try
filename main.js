0b82595845794feaac72b3da516fad40f2538ae4
MHM <hopemrew@gmail.com>
2021-08-04 17:39:20 +0200

    tip calculator section

--- main.js
+++ main.js
@@ -50,3 +50,16 @@ btns.forEach(function (btn) {
 
 })
 
+// Tip calculator
+
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+    let billAmount = document.getElementById('bill-amount').value;
+    let tipPercentage = document.getElementById('tip-percentage').value;
+
+    
+    let tipAmount = document.getElementById('tip-amount').value = billAmount / tipPercentage;
+    document.getElementById('total-bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+
+})
