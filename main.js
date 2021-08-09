0440c5e0fdd1e7b32dc03561006661012c4c97fb
MHM <hopemrew@gmail.com>
2021-08-09 19:16:16 +0200

    blury back ground page

--- main.js
+++ main.js
@@ -65,3 +65,24 @@ btn.addEventListener('click', function(){
     
 
 })
+
+// blury backgdoun loading
+
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+  load++
+
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
